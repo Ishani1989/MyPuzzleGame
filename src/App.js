d37993a31f5f21ff9feb5b26a3995031232d785f
Ishani Mukherjee <ishanibhattacharjee@gmail.com>
2017-09-02 22:22:14 -0700

    added app intro to app.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>The Puzzle Game</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         This app will allow you to play a puzzle game with any pic of your choice or some pre selected pictures from our directory.
         </p>
       </div>
     );
